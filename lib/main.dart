cd363107defa84c3dd288a144a319a5ea6523191
CSW <christianweaves@gmail.com>
2020-09-08 00:20:27 +0100

    CWM-3 Grabbing features from the rest endpoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+import 'providers/articles.dart';
 
 void main() {
   runApp(ChristianWeavesDotComApp());
 }
 
 class ChristianWeavesDotComApp extends StatelessWidget {
-  // This widget is the root of your application.
+  @override
+  void initState() {}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Christian Weaves App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: TheHomePage(title: 'Christian Weaves dot com'),
@@ -24,16 +25,6 @@ class ChristianWeavesDotComApp extends StatelessWidget {
 
 class TheHomePage extends StatefulWidget {
   TheHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -43,17 +34,17 @@ class TheHomePage extends StatefulWidget {
 class _TheHomePageState extends State<TheHomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          RaisedButton(
+            child: Text('Test button'),
+            onPressed: () => print(Articles().getFeaturedArticle()),
+          )
+        ],
       ),
     );
   }
